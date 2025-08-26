00bdea327ca689115cca7100543def0a65731f95
Lutmila <lutmilagaray57@gmail.com>
2025-08-25 22:10:15 -0300

    validacion de contador

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,20 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+
+
 
   return (
     <>
     <h1 className='title'>Contador</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count - 1)} > - </button>
-
+        <button disabled={count === 0} onClick={count === 0 ? () => {} : () => setCount((count) => count - 1)} > Restar </button>
+        <p className="texto-contador">
          {count}
-        <button onClick={() => setCount((count) => count + 1)} > + </button>
           
+        </p>
+        <button onClick={() => setCount((count) => count + 1)} > Sumar </button>
       </div>
     </>
   )
