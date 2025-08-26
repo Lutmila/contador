6483ced751c9f60664ee670f3a6c7ccbc3883420
Lutmila <lutmilagaray57@gmail.com>
2025-08-25 22:02:32 -0300

    Creacion de contador

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <h1 className='title'>Contador</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count - 1)} > - </button>
+
+         {count}
+        <button onClick={() => setCount((count) => count + 1)} > + </button>
+          
+      </div>
+    </>
+  )
+}
+
+export default App
